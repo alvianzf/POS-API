c3ab460c2e901a8b2dd1b43e5195e046957c19c5
unknown <fiqra22@gmail.com>
2023-10-05 01:42:53 +0700

    feat: add get one category by id 1. Add endpoint for get category by id
    Create a get one category endpoint at /category/:id #4

--- routes/api/menu/index.js
+++ routes/api/menu/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//get by id category
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const menu = await Menu.findOne(id);
+
+    res.status(200).json({
+      message: "Success",
+      data: menu,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "incorrect id",
+      error: error,
+    });
+  }
 });
 
 module.exports = router;
